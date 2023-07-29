925f7d74f3c7e910f96178a0b8ebc8757f450a22
Jigar <jigarrathod8989@gmail.com>
2023-07-29 13:21:59 +0530

    Employee comp

---
+++ reactjs/src/Employee.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class Employee extends Component {
+  static propTypes = {second: third}
+
+  render() {
+    return (
+      <div>Employee</div>
+    )
+  }
+}
