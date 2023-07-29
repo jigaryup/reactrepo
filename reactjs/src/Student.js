5b0c852b9f2cbfc87e1f7bfeacb8aa8deb085d38
Jigar <jigarrathod8989@gmail.com>
2023-07-29 12:52:38 +0530

    student comp

---
+++ reactjs/src/Student.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Student(props) {
+  return (
+    <div>Student</div>
+  )
+}
+
+Student.propTypes = {}
+
+export default Student
